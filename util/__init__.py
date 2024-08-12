d007b8a24f96b116e001d362361f9ef55593aceb
manastarla <manastarla@hotmail.com>
2024-08-12 18:05:12 -0300

    Add files via upload

---
+++ util/__init__.py
@@ -0,0 +1,18 @@
+"""A Python library for Dynamic Ensemble Selection.
+
+``SGPDESlib`` is a library containing the implementation of the state-of-the art
+dynamic classifier and ensemble selection techniques. The library also provides
+some static ensemble methods that are used as baseline comparison.
+
+Subpackages
+-----------
+
+util
+    A collection of aggregation functions and diversity measures for ensemble
+    of classifiers.
+"""
+
+# list of all modules available in the library
+__all__ = ['poolgenerator']
+
+__version__ = '0.0.1'
